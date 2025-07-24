9e2dcf05e8dcc2b68f88eba2cc38456005b4e774
keepinfov <218463407+keepinfov@users.noreply.github.com>
2025-07-24 22:41:31 +0300

    io custom module added examples

--- src/lib.rs
+++ src/lib.rs
@@ -30,14 +30,35 @@
 //!     } else {
 //!         println!("Selection cancelled.");
 //!     }
+//!
+//!     let ic = InputConfig {
+//!         input_text_color: Color::Blue,
+//!         ..Default::default()
+//!     };
+//!     let name = read_input(&ic)?;
+//!     // Without log level:
+//!     let oc = OutputConfig {
+//!         log_level: None,
+//!         ..Default::default()
+//!     };
+//!     write_output(&oc, &format!("Hello, {}!", name))?;
+//!     // With log level:
+//!     let oc2 = OutputConfig {
+//!         log_level: Some("DEBUG".into()),
+//!         ..Default::default()
+//!     };
+//!     write_output(&oc2, "This is a debug message.")?;
+//!
 //!     Ok(())
 //! }
 //! ```
 
 pub mod color;
+pub mod io;
 pub mod list;
 
 pub mod prelude {
     pub use super::color::*;
+    pub use super::io::*;
     pub use super::list::*;
 }
--- src/main.rs
+++ src/main.rs
@@ -18,5 +18,24 @@ fn main() -> anyhow::Result<()> {
     } else {
         println!("Selection cancelled.");
     }
+
+    let ic = InputConfig {
+        input_text_color: Color::Blue,
+        ..Default::default()
+    };
+    let name = read_input(&ic)?;
+    // Without log level:
+    let oc = OutputConfig {
+        log_level: None,
+        ..Default::default()
+    };
+    write_output(&oc, &format!("Hello, {}!", name))?;
+    // With log level:
+    let oc2 = OutputConfig {
+        log_level: Some("DEBUG".into()),
+        ..Default::default()
+    };
+    write_output(&oc2, "This is a debug message.")?;
+
     Ok(())
 }
